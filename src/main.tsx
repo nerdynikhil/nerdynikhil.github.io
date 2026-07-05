809c6bdcfcf0ddc4cdf9e0b3517022ab355033e8
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2026-07-05 19:36:05 +0530

    feat: integrate vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <HelmetProvider>
       <BrowserRouter>
         <App />
+        <Analytics />
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
